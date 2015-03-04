d16aac64491d7a2fccb57cdaac6105f30f456d81
kpell3 <kpell3@uis.edu>
2015-03-03 23:47:04 -0600

    Missed Host.cs when I added the skeleton.

---
+++ GoingViral/GoingViral/Host.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoingViral
+{
+    class Host
+    {
+        Host()
+        {
+            
+        }
+
+        
+        public void Infect()
+        {
+
+        }
+        public Virus InfectedBy;
+        public double Progression;
+        public bool Quarentined;
+        public double Resistance;
+        public double NumberOfPeopleRepresentedByThisHost;
+    }
+}
