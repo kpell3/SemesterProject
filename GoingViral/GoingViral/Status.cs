259413f1f72d90c22cacbb1791df2809f4d7a3cc
kpell3 <kpell3@uis.edu>
2015-03-07 22:47:06 -0600

    Added a class for the Status of a host so that a host could return its own status and the location could know if the host needed to be disposed of.

---
+++ GoingViral/GoingViral/Status.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoingViral
+{
+    /// <summary>
+    /// This is an enum representing the Status of a host.
+    /// A host can be either Uninfected, Dead, or Still Infected
+    /// </summary>
+    enum Status
+    {
+        /// <summary>
+        /// The host is not infected. This signals removal of the host
+        /// from tracking without increasing the dead count.
+        /// </summary>
+        Uninfected,
+        /// <summary>
+        /// This host is dead. It should be removed from the list and added
+        /// to the dead count.
+        /// </summary>
+        Dead,
+        /// <summary>
+        /// This host is still infected. It should continue to be tracked.
+        /// </summary>
+        StillInfected
+    }
+}
