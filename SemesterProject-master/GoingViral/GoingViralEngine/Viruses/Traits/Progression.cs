a3572fa3990c904fa619e7508d13fa72d6bfb85d
tpodlipni <tpodlipni@gmail.com>
2015-04-11 22:08:50 -0500

    Added MapInformationControl that will display a map and information about regions. Also, refactored solution into a UI and engine portion.  This will ensure that when the solution is built that the engine is built first and then the application will be executed.  I tested my changes and I was able to build the project and no errors (woohoo!).

---
+++ SemesterProject-master/GoingViral/GoingViralEngine/Viruses/Traits/Progression.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoingViralEngine.Viruses.Traits
+{
+    /// <summary>
+    /// Represents how long symptoms stay active in a host
+    /// </summary>
+    public enum Progression
+    {
+        /// <summary>
+        /// Symptoms come and go within a day or so.
+        /// </summary>
+        Fleeting,
+
+        /// <summary>
+        /// Symptoms come and stay for about a week, but go away after that.
+        /// </summary>
+        Acute,
+
+        /// <summary>
+        /// Symptoms come and the host continues to suffer for months or years,
+        /// but given enough time may recover.
+        /// </summary>
+        Chronic,
+
+        /// <summary>
+        /// Symptoms are with the host until the host dies.
+        /// </summary>
+        Terminal
+    }
+}
